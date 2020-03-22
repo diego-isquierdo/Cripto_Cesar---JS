cf1d731868c7b989edd8f1b7789dd59b5879d202
Diego Isquierdo <diego.isquierdo@hotmail.com>
2020-03-22 17:29:47 -0300

    First Commit

---
+++ js/buscar.js
@@ -0,0 +1,9 @@
+var botaoBuscar = document.querySelector("#buscarJSON");
+
+botaoBuscar.addEventListener("click", ()=>{
+    var inputGenerate = document.querySelector("#inputGenerate");
+    var linkGenerate = inputGenerate.value;
+
+    console.log(linkGenerate);
+
+});
